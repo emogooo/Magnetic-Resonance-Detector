efb0f13ccdca244b75bad7f223f60df2475c5b7c
Emirhan Aydın <58745898+emogooo@users.noreply.github.com>
2022-09-19 15:45:49 +0300

    Update Yeralti-Madde-Dedektoru.ino

--- Yeralti-Madde-Dedektoru.ino
+++ Yeralti-Madde-Dedektoru.ino
@@ -9,8 +9,8 @@
 #define wifiPin A1
 #define lowBatteryModePin A2
 
-AD9833 signalGenerator(FSyncPin);  
-LiquidCrystal lcd(7,6,5,4,3,2);
+AD9833 signalGenerator(FSyncPin);
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 int menuVar;
 String result;
@@ -19,10 +19,10 @@ bool wifiFlag;
 double lastVoltage;
 byte batLevel[8];
 
-void setup(){
-  lcd.begin(20,4);
+void setup() {
+  lcd.begin(20, 4);
   lcd.print("  DELTA ELEKTRONiK");
-  lcd.setCursor(0,2);
+  lcd.setCursor(0, 2);
   lcd.print(" ALAN TARAMA CiHAZI");
   delay(4000);
   lcd.clear();
@@ -43,120 +43,106 @@ void setup(){
   signalGenerator.Begin();
 }
 
-void loop(){
-  if(!wifiFlag && digitalRead(wifiPin)){
-    lcd.setCursor(15,3);
+void loop() {
+  if (!wifiFlag && digitalRead(wifiPin)) {
+    lcd.setCursor(15, 3);
     lcd.print("((O))");
     wifiFlag = true;
-  }else if(wifiFlag && !digitalRead(wifiPin)){
-    lcd.setCursor(15,3);
+  } else if (wifiFlag && !digitalRead(wifiPin)) {
+    lcd.setCursor(15, 3);
     lcd.print("     ");
     wifiFlag = false;
   }
-  
+
   double voltage = 4.1;
-  if(abs(voltage - lastVoltage) > 1 ){
+  if (abs(voltage - lastVoltage) > 1) {
     batterylevel(voltage);
     lastVoltage = voltage;
-    if(voltage > 3 && !digitalRead(lowBatteryModePin)){
+    if (voltage > 3 && !digitalRead(lowBatteryModePin)) {
       digitalWrite(lowBatteryModePin, HIGH);
-    }else if(voltage < 3 && digitalRead(lowBatteryModePin)){
+    } else if (voltage < 3 && digitalRead(lowBatteryModePin)) {
       digitalWrite(lowBatteryModePin, LOW);
     }
   }
-  
-  if(digitalRead(buttonRightPin) && menuFlag){
+
+  if (digitalRead(buttonRightPin) && menuFlag) {
     menuVar += 1;
-    if(menuVar > 8){
+    if (menuVar > 8) {
       menuVar = 1;
     }
     result = getMenuString();
     yaz("Seciniz " + result, 2);
     delay(250);
   }
-  
-  if(digitalRead(buttonLeftPin) && menuFlag){
+
+  if (digitalRead(buttonLeftPin) && menuFlag) {
     menuVar -= 1;
-    if(menuVar < 1){
+    if (menuVar < 1) {
       menuVar = 8;
     }
     result = getMenuString();
     yaz("Seciniz " + result, 2);
     delay(250);
   }
-  
-  if(digitalRead(buttonEnterPin)){
+
+  if (digitalRead(buttonEnterPin)) {
     menuFlag = !menuFlag;
-    if(!menuFlag ){
+    if (!menuFlag) {
       yaz("", 1);
       yaz(result, 2);
       signalGenerator.ApplySignal(SINE_WAVE, REG0, getFrequency());
-      signalGenerator.EnableOutput(true); 
-    }else{
+      signalGenerator.EnableOutput(true);
+    } else {
       yaz("", 2);
       yaz("Seciniz " + result, 2);
-      signalGenerator.EnableOutput(false); 
+      signalGenerator.EnableOutput(false);
     }
     delay(250);
   }
 }
 
-String getMenuString(){
-   if(menuVar == 1){
-      return "ALTIN";
-    }
-    else if(menuVar == 2){
-      return "GUMUS";
-    }
-    else if(menuVar == 3){
-      return "BRONZ";
-    }
-    else if(menuVar == 4){
-      return "BAKIR";
-    }
-    else if(menuVar == 5){
-      return "DEMiR";
-    }
-    else if(menuVar == 6){
-      return "BOSLUK";
-    }
-    else if(menuVar == 7){
-      return "KEMiK";
-    }
-    else if(menuVar == 8){
-      return "  SU  ";
-    }
+String getMenuString() {
+  if (menuVar == 1) {
+    return "ALTIN";
+  } else if (menuVar == 2) {
+    return "GUMUS";
+  } else if (menuVar == 3) {
+    return "BRONZ";
+  } else if (menuVar == 4) {
+    return "BAKIR";
+  } else if (menuVar == 5) {
+    return "DEMiR";
+  } else if (menuVar == 6) {
+    return "BOSLUK";
+  } else if (menuVar == 7) {
+    return "KEMiK";
+  } else if (menuVar == 8) {
+    return "  SU  ";
+  }
 }
 
-int getFrequency(){
-   if(menuVar == 1){
-      return 1;
-    }
-    else if(menuVar == 2){
-      return 2;
-    }
-    else if(menuVar == 3){
-      return 3;
-    }
-    else if(menuVar == 4){
-      return 4;
-    }
-    else if(menuVar == 5){
-      return 5;
-    }
-    else if(menuVar == 6){
-      return 6;
-    }
-    else if(menuVar == 7){
-      return 7;
-    }
-    else if(menuVar == 8){
-      return 8;
-    }
+int getFrequency() {
+  if (menuVar == 1) {
+    return 1;
+  } else if (menuVar == 2) {
+    return 2;
+  } else if (menuVar == 3) {
+    return 3;
+  } else if (menuVar == 4) {
+    return 4;
+  } else if (menuVar == 5) {
+    return 5;
+  } else if (menuVar == 6) {
+    return 6;
+  } else if (menuVar == 7) {
+    return 7;
+  } else if (menuVar == 8) {
+    return 8;
+  }
 }
 
-void yaz(String yazi, byte satir){
-  for(int i = 0; i < 20; i++){
+void yaz(String yazi, byte satir) {
+  for (int i = 0; i < 20; i++) {
     lcd.setCursor(i, (satir % 4));
     lcd.print(" ");
   }
@@ -164,8 +150,8 @@ void yaz(String yazi, byte satir){
   lcd.print(yazi);
 }
 
-void solaYaz(String yazi, byte satir){
-  for(int i = 0; i < 20; i++){
+void solaYaz(String yazi, byte satir) {
+  for (int i = 0; i < 20; i++) {
     lcd.setCursor(i, (satir % 4));
     lcd.print(" ");
   }
@@ -173,23 +159,21 @@ void solaYaz(String yazi, byte satir){
   lcd.print(yazi);
 }
 
-double getVoltage(){
-  int x = random(0,3);
-  if(x == 0){
+double getVoltage() {
+  int x = random(0, 3);
+  if (x == 0) {
     return 11.59;
-  }else if(x == 1){
+  } else if (x == 1) {
     return 12.76;
-  }else if(x == 2){
+  } else if (x == 2) {
     return 11.88;
-  }else{
+  } else {
     return 12.37;
   }
 }
 
-void batterylevel(double curvolt)
-{
-  if(curvolt <= 4.2 && curvolt > 4.0)
-  {
+void batterylevel(double curvolt) {
+  if (curvolt <= 4.2 && curvolt > 4.0) {
     batLevel[1] = B11111;
     batLevel[2] = B11111;
     batLevel[3] = B11111;
@@ -198,8 +182,7 @@ void batterylevel(double curvolt)
     batLevel[6] = B11111;
     batLevel[7] = B11111;
   }
-  if(curvolt <= 4.0 && curvolt > 3.8)
-  {
+  if (curvolt <= 4.0 && curvolt > 3.8) {
     batLevel[1] = B10001;
     batLevel[2] = B11111;
     batLevel[3] = B11111;
@@ -208,18 +191,16 @@ void batterylevel(double curvolt)
     batLevel[6] = B11111;
     batLevel[7] = B11111;
   }
-  if(curvolt <= 3.8 && curvolt > 3.6)
-  {
+  if (curvolt <= 3.8 && curvolt > 3.6) {
     batLevel[1] = B10001;
     batLevel[2] = B10001;
     batLevel[3] = B11111;
     batLevel[4] = B11111;
     batLevel[5] = B11111;
     batLevel[6] = B11111;
-    batLevel[7] = B11111;  
+    batLevel[7] = B11111;
   }
-  if(curvolt <= 3.6 && curvolt > 3.4)
-  {
+  if (curvolt <= 3.6 && curvolt > 3.4) {
     batLevel[1] = B10001;
     batLevel[2] = B10001;
     batLevel[3] = B10001;
@@ -228,8 +209,7 @@ void batterylevel(double curvolt)
     batLevel[6] = B11111;
     batLevel[7] = B11111;
   }
-  if(curvolt <= 3.4 && curvolt > 3.2)
-  {
+  if (curvolt <= 3.4 && curvolt > 3.2) {
     batLevel[1] = B10001;
     batLevel[2] = B10001;
     batLevel[3] = B10001;
@@ -238,8 +218,7 @@ void batterylevel(double curvolt)
     batLevel[6] = B11111;
     batLevel[7] = B11111;
   }
-  if(curvolt <= 3.2 && curvolt > 3.0)
-  {
+  if (curvolt <= 3.2 && curvolt > 3.0) {
     batLevel[1] = B10001;
     batLevel[2] = B10001;
     batLevel[3] = B10001;
@@ -248,8 +227,7 @@ void batterylevel(double curvolt)
     batLevel[6] = B11111;
     batLevel[7] = B11111;
   }
-  if(curvolt < 3.0)
-  {
+  if (curvolt < 3.0) {
     batLevel[1] = B10001;
     batLevel[2] = B10001;
     batLevel[3] = B10001;
@@ -258,7 +236,7 @@ void batterylevel(double curvolt)
     batLevel[6] = B10001;
     batLevel[7] = B11111;
   }
-  lcd.createChar(0 , batLevel);
-  lcd.setCursor(0,3);
+  lcd.createChar(0, batLevel);
+  lcd.setCursor(0, 3);
   lcd.write(byte(0));
 }
